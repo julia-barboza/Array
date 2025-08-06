2de20c56d19e09b9f446c21777435aacf0dc70a7
júlia barboza <barboza.juliag@gmail.com>
2025-08-06 12:13:21 -0300

    Add files via upload

--- Array1.java
+++ Array1.java
@@ -1,9 +1,27 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Array1 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner scanner = new Scanner(System.in);
+		ArrayList<Integer> numeros = new ArrayList<>();
 
+		System.out.println("Digite números inteiros (digite -1 para encerrar):");
+		int numero;
+		do {
+			numero = scanner.nextInt();
+			if (numero != -1) {
+				numeros.add(numero);
+			}
+		} while (numero != -1);
+
+		int soma = 0;
+		for (int n : numeros) {
+			soma += n;
+		}
+
+		System.out.println("A soma dos números é: " + soma);
+		scanner.close();
 	}
-
 }
--- Array2.java
+++ Array2.java
@@ -1,4 +1,28 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Array2 {
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		ArrayList<Integer> numeros = new ArrayList<>();
+		
+		System.out.println("Digite números inteiros (digite -1 para encerrar):");
+		int numero;
+		do {
+			numero = scanner.nextInt();
+			if (numero != -1) {
+				numeros.add(numero);
+			}
+		} while (numero != -1);
 
+		System.out.println("Números pares da lista:");
+		for (int n : numeros) {
+			if (n % 2 == 0) {
+				System.out.println(n);
+			}
+		}
+
+		scanner.close();
+	}
 }
